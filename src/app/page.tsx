2e3d4f9c23e60139536423b814cabc56364b18aa
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-02-02 17:24:36 +0300

    bugfix: hydration error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import MultipleSelect from "@/components/MultipleSelect";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <MultipleSelect />
+      {isClient && <MultipleSelect />}
     </main>
   );
 }
