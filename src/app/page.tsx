c66cfdaab4f95a3d8a262fb8c774ecabbc8245af
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-02-02 16:43:12 +0300

    add MultipleSelect Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import MultipleSelectSWR from "@/components/MulipleSelectSWR";
 import MultipleSelect from "@/components/MultipleSelect";
+import MultipleSelectReactQuery from "@/components/MultipleSelectReactQuery";
 
 export default function Home() {
   return (
     <main>
       <MultipleSelect />
+
+      <MultipleSelectSWR />
     </main>
   );
 }
