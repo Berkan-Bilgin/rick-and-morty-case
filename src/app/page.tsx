716952de5e4ad5ed8d4b80d92eb7064b8f51a780
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-02-02 17:13:00 +0300

    feat: add styles centered component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MultipleSelect from "@/components/MultipleSelect";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex justify-center items-center min-h-screen">
       <MultipleSelect />
     </main>
   );
